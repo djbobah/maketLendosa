d5e92590ed632b68d2df05dff3a35b78abb12d41
Vladimir Kirpichev <djbobah@mail.ru>
2024-01-09 23:52:09 +0300

    error with ul text

--- index.js
+++ index.js
@@ -136,3 +136,129 @@ menuLink.innerText = "Смотреть все";
 filterMenu.append(menuLink);
 
 sectionPopular.append(filterMenu);
+
+const cardsContainer = document.createElement("div");
+cardsContainer.className = "cards-container";
+
+// возможно нужно добавить колонки
+
+houses.map((house) => {
+  const popularCard = document.createElement("div");
+  popularCard.className = "porular__card popular-card_bg";
+  const popularCardBody = document.createElement("div");
+  popularCardBody.className = "pporular-card__body";
+
+  const popularCardTitle = document.createElement("h3");
+  popularCardTitle.className = "porular-card__title";
+  popularCardTitle.innerText = house.name;
+  popularCardBody.append(popularCardTitle);
+
+  const popularCardDescriptionCol = document.createElement("div");
+  popularCardDescriptionCol.className = "popular-card-description__col";
+
+  const popularCardDescription = document.createElement("div");
+  popularCardDescription.className = "popular-card__description";
+  popularCardDescription.innerText = " Площадь:";
+
+  const popularCardDescriptionTextBold = document.createElement("span");
+  popularCardDescriptionTextBold.className =
+    "popular-card-description__text_bold";
+  popularCardDescriptionTextBold.innerText = house.square;
+  popularCardDescription.append(popularCardDescriptionTextBold);
+
+  popularCardDescriptionCol.append(popularCardDescription);
+
+  const popularCardDescription2 = document.createElement("div");
+  popularCardDescription2.className = "popular-card__description";
+  popularCardDescription2.innerText = " Размеры:";
+
+  const popularCardDescriptionTextBold2 = document.createElement("span");
+  popularCardDescriptionTextBold2.className =
+    "popular-card-description__text_bold";
+  popularCardDescriptionTextBold2.innerText = house.sizes;
+  popularCardDescription2.append(popularCardDescriptionTextBold2);
+
+  popularCardDescriptionCol.append(popularCardDescription2);
+  popularCardBody.append(popularCardDescriptionCol);
+
+  const popularCardDescriptionCol2 = document.createElement("div");
+  popularCardDescriptionCol2.className = "popular-card-description__col";
+
+  const popularCardDescription3 = document.createElement("div");
+  popularCardDescription3.className = "popular-card__description";
+  popularCardDescription3.innerText = "Срок стройки:";
+
+  const popularCardDescriptionTextBold3 = document.createElement("span");
+  popularCardDescriptionTextBold3.className =
+    "popular-card-description__text_bold";
+  popularCardDescriptionTextBold3.innerText = house.constructionPeriod;
+  popularCardDescription3.append(popularCardDescriptionTextBold3);
+
+  popularCardDescriptionCol2.append(popularCardDescription3);
+
+  const popularCardDescription4 = document.createElement("div");
+  popularCardDescription4.className = "popular-card__description";
+  popularCardDescription4.innerText = " Комнат:";
+
+  const popularCardDescriptionTextBold4 = document.createElement("span");
+  popularCardDescriptionTextBold4.className =
+    "popular-card-description__text_bold";
+  popularCardDescriptionTextBold4.innerText = house.rooms;
+  popularCardDescription4.append(popularCardDescriptionTextBold2);
+
+  popularCardDescriptionCol2.append(popularCardDescription4);
+  popularCardBody.append(popularCardDescriptionCol2);
+
+  const popularCardDescriptionList = document.createElement("ul");
+  popularCardDescriptionList.className = "popular-card-description__list";
+  popularCardDescriptionList.innerHTML = "<li>dsfsf</li>";
+
+  // const popularCardDescriptionListItem = document.createElement("li");
+  // popularCardDescriptionListItem.className =
+  //   "popular-card-description-list__item";
+  // popularCardDescriptionListItem.innerText = "Тип дома:";
+  // const popularCardDescriptionTextBoldRight = document.createElement("span");
+  // popularCardDescriptionTextBoldRight.innerText = house.type;
+
+  // popularCardDescriptionListItem.append(popularCardDescriptionTextBoldRight);
+
+  // popularCardDescriptionList.append(popularCardDescriptionListItem);
+
+  // const popularCardDescriptionListItem2 = document.createElement("li");
+  // popularCardDescriptionListItem2.className =
+  //   "popular-card-description-list__item";
+  // const popularCardDescriptionListItemBigHeight = document.createElement("div");
+  // popularCardDescriptionListItemBigHeight.className =
+  //   "popular-card-description-list__item_bigHeight";
+
+  // const popularCardDescriptionTextBottom = document.createElement("span");
+  // popularCardDescriptionTextBottom.className =
+  //   "popular-card-description__text_bottom";
+  // popularCardDescriptionTextBottom.innerText = "Стоимость:";
+  // popularCardDescriptionListItemBigHeight.append(
+  //   popularCardDescriptionTextBottom
+  // );
+  // const popularCardDescriptionTextBoldRight2 = document.createElement("span");
+  // popularCardDescriptionTextBoldRight2.className =
+  //   "popular-card-description__text_bold-right";
+  // // popularCardDescriptionTextBoldRight2.innerText = " ";
+  // popularCardDescriptionTextBoldRight2.innerHTML +=
+  //   "от" +
+  //   ` \<span class=\"popular-card-description__price\" \>${house.price}\<\/span\> руб\<\/span\>`;
+  // popularCardDescriptionListItemBigHeight.append(
+  //   popularCardDescriptionTextBoldRight2
+  // );
+
+  // popularCardDescriptionListItem2.append(
+  //   popularCardDescriptionListItemBigHeight
+  // );
+
+  // popularCardDescriptionList.appendChild(popularCardDescriptionListItem2);
+
+  popularCardBody.append(popularCardDescriptionList);
+
+  popularCard.append(popularCardBody);
+  cardsContainer.append(popularCard);
+});
+sectionPopular.append(cardsContainer);
+// sectionPopular.append(cardsContainer)
